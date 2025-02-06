b35d76dfd36fac5b4fd721080b884a50b3ccfcc8
Duygu Tuncay <duygutuncay@hotmail.com>
2025-02-06 20:59:30 +0300

    TRAIN-33 Verilerin eğitim, test ve bağımsız değişken, bağımlı değişken olarak bölünmesi

--- ml0005.py
+++ ml0005.py
@@ -13,39 +13,30 @@ print(datas)
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 yas = datas.iloc[:,1:4].values
-# print(yas)
 
 #ogren
 imputer = imputer.fit(yas[:,1:4])
 #ortalama deger olarak yerine koy
 yas[:,1:4]=imputer.transform(yas[:,1:4])
-# print(yas)
 
 # kategorik veriler
 countries = datas.iloc[:,0:1].values
-# print(countries)
 
 from sklearn import preprocessing
 le = preprocessing.LabelEncoder()
 countries[:,0] = le.fit_transform(datas.iloc[:,0])
-# print(countries)
+
 
 # polynominal kategorik verinin ayrı değişkenler gibi true - false gibi ayrı sütunlara dönüştürülmesi
 ohe = preprocessing.OneHotEncoder()
 countries = ohe.fit_transform(countries).toarray()
-# print(countries)
 
 # Sonuçların 1 dataframe'de toplanması
 result = pd.DataFrame(data=countries, index=range(22), columns=['fr','tr','us'])
-print(result)
-
 result2 = pd.DataFrame(data=yas, index=range(22), columns=['boy', 'kilo', 'yas'])
-print(result2)
-
 cinsiyet = datas.iloc[:,-1].values
-print(cinsiyet)
 result3 = pd.DataFrame(data=cinsiyet, index=range(22), columns=['cinsiyet'])
-print(result3)
+
 
 # dataframe'lerin birleştirilmesi
 # s = pd.concat([result,result2])
@@ -54,3 +45,16 @@ print(s)
 
 s2 = pd.concat([s, result3], axis=1)
 print(s2)
+
+# Veri Kaynağını Bölme
+# Boy, kilo ve yaştan cinsiyetin tahmin edilebilmesini istiyoruz.
+# verilerin train ve test olarak ve
+# bağımlı değişkeni (hedefi) ayırıyoruz. x : bağımsız değişkenler, y: bağımlı değişkenler
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(s, result3,test_size=0.33,random_state=0)
+# veriyi 4'e böldük.
+print(x_train)
+print(y_train)
+print(x_test)
+print(y_test)
+
